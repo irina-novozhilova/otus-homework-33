54bcf6dfe2c2816e18acfb2e6c0ff8a57b17ea31
Irina Novozhilova <irina@nsvision.com>
2023-05-22 17:37:11 +0400

    add settings

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = (api) => ({
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: "3.22",
+        targets: api.caller((caller) => caller && caller.target === "node")
+          ? { node: "current" }
+          : { chrome: "58", ie: "11" },
+      },
+      "@babel/preset-typescript",
+    ],
+  ],
+});
